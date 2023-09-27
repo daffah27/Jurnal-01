80ad1edd133ac3d5d9d0632567c411b6f8e0c2cd
Daffah <Muh.daffah27@gmail.com>
2023-09-27 09:02:48 +0700

    Upload Pertama

---
+++ Jurnal.java
@@ -0,0 +1,7 @@
+public class Jurnal {
+    public static void main(String[] args){
+        System.out.println("Nama: Muh Daffah Putra Muharsyah");
+        System.out.println("NIM: 607062330131");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
